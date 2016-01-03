027a11bb9b85dd2fb0e5af0418eabca9d6ff3f9a
Marcus Erlandsson <mail@marquz.com>
2016-01-03 21:35:25 +0100

    Implemented adding goals, listing them and storing them in local storage.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # doit
+
+# TODO:
+- GUI for Adding & seeing goals (weekly)
+- Data for storing goals
+- GUI for adding activity to goal.
--- public/app.js
+++ public/app.js
@@ -13,19 +13,90 @@ dashApp.config(['$routeProvider',
         }
     ]
 );
-dashApp.service('FooService', function($http) {
-    var epics = {};
-    return {
-        get_epics: function() {
-            return epics;
-        },
-        set_epics: function(_epics) {
-            epics = _epics;
+dashApp.service('GoalService', function($http) {
+    function generateUUID(){
+        var d = new Date().getTime();
+        if(window.performance && typeof window.performance.now === "function"){
+            d += performance.now();; //use high-precision timer if available
         }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
+    var goal_skeleton = {
+        'id': '',
+        name: '',
+        frequency: 1,
+        duration: 'week'
+    };
+    function addGoal(params) {
+        goals.push(
+            {
+                'id': generateUUID(),
+                name: params['name'],
+                frequency: params['frequency'],
+                duration: params['duration']
+            }
+        );
+        storeGoalsToDisk();
+    }
+    function listGoals() {
+        if (goals.length === 0) {
+            goals = fetchGoalsFromDisk();
+        }
+        return goals;
+    }
+    function clearGoals() {
+        goals = [];
+        storeGoalsToDisk();
+    }
+    function storeGoalsToDisk() {
+        localStorage.setItem('doit:goals', JSON.stringify(goals));
+    }
+    function fetchGoalsFromDisk() {
+        var fetched_goals = localStorage.getItem('doit:goals');
+        if (fetched_goals) {
+            return JSON.parse(fetched_goals);
+        }
+        return [];
+    }
+
+    var goals = [];
+    return {
+        addGoal: addGoal,
+        listGoals: listGoals,
+        clearGoals: clearGoals
     };
 });
 
-dashApp.controller('DashboardController', function($scope) {
-   $scope.foo = 'Hej!';
+dashApp.controller('DashboardController', function($scope, GoalService) {
+    $scope.STATE_ADD_GOAL = 'ADD_GOAL';
+    $scope.state = null;
+
+    $scope.showAddGoal = function() {
+        $scope.state = $scope.STATE_ADD_GOAL;
+    };
+    $scope.cancelAddGoal = function() {
+        $scope.state = null;
+    };
+    $scope.addGoal = function() {
+        console.log('Add goal', $scope.add_goal_model);
+        GoalService.addGoal($scope.add_goal_model);
+        $scope.state = null;
+        $scope.goals = GoalService.listGoals();
+    };
+    $scope.clearGoals = function() {
+        GoalService.clearGoals();
+        init();
+    };
+
+    function init() {
+        $scope.goals = GoalService.listGoals();
+    }
+
+    init();
 });
 
