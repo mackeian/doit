722f54c191bc09ca5dadbf5332f9fe250a356397
Marcus Erlandsson <mail@marquz.com>
2016-01-04 20:22:57 +0100

    Implemented activity adding/removing and custom adding of activity on specific date. Displaying all activities on a goal right now.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # doit
 
 # TODO:
-- GUI for Adding & seeing goals (weekly)
-- Data for storing goals
-- GUI for adding activity to goal.
+- Showing goals with activity stats for the current duration (do it lazy with angular $scope.doesGoalH)
--- public/app.js
+++ public/app.js
@@ -13,7 +13,10 @@ dashApp.config(['$routeProvider',
         }
     ]
 );
-dashApp.service('GoalService', function($http) {
+dashApp.service('lodash', function() {
+   return _;
+});
+dashApp.service('GoalService', function($http, lodash) {
     function generateUUID(){
         var d = new Date().getTime();
         if(window.performance && typeof window.performance.now === "function"){
@@ -43,18 +46,93 @@ dashApp.service('GoalService', function($http) {
         );
         storeGoalsToDisk();
     }
+    function preProcessGoals(goals) {
+        var today_date = _serializerDate(new Date());
+        lodash.forEach(goals, function(goal) {
+            var hasActivityToday =  lodash.find(goal.activities, function(activity) {
+               return activity.date_added === today_date;
+            });
+            goal._has_activity_today = !!hasActivityToday;
+        });
+    }
+    function preSaveWorkOnGoals(goals) {
+        lodash.forEach(goals, function(goal) {
+            delete goal['_has_activity_today'];
+        });
+    }
+
     function listGoals() {
         if (goals.length === 0) {
             goals = fetchGoalsFromDisk();
         }
+        preProcessGoals(goals);
         return goals;
     }
     function clearGoals() {
         goals = [];
         storeGoalsToDisk();
     }
+
+    function _serializerDate(date) {
+        return date.toISOString().slice(0,10).replace(/-/g,"");
+    }
+
+    /* Activity */
+    function addActivityOnGoal(goal, date_to_add) {
+        if (date_to_add === undefined) {
+            date_to_add = new Date();
+        }
+        date_to_add = _serializerDate(date_to_add);
+        var date_today = _serializerDate(new Date());
+
+        if (goal.activities === undefined) {
+            goal.activities = [];
+        }
+
+        var activity_already_on_date = lodash.find(goal.activities, function(activity) {
+                return activity.date_added === date_to_add;
+            }
+        );
+        if (activity_already_on_date) {
+            return false;
+        }
+        goal.activities.push(
+            {
+                date_added: date_to_add,
+            }
+        );
+        goal._has_activity_today = goal._has_activity_today || date_to_add === date_today;
+        storeGoalsToDisk();
+        return true;
+    }
+
+    function removeActivityOnGoal(goal, date_to_remove) {
+        if (!goal.activities) {
+            return false;
+        }
+
+        if (date_to_remove === undefined) {
+            date_to_remove = new Date();
+        }
+        date_to_remove = _serializerDate(date_to_remove);
+        var date_today = _serializerDate(new Date());
+        var index_of_activity_on_date = lodash.findIndex(goal.activities, function(activity) {
+           return activity.date_added === date_to_remove;
+        });
+        if (index_of_activity_on_date >= 0) {
+            goal.activities.splice(index_of_activity_on_date, 1);
+            if (date_to_remove === date_today) {
+                goal._has_activity_today = false;
+            }
+            storeGoalsToDisk();
+            return true;
+        }
+        return false;
+    }
+
+    /* Storage */
     function storeGoalsToDisk() {
-        localStorage.setItem('doit:goals', JSON.stringify(goals));
+        localStorage.setItem('doit:goals', angular.toJson(goals));
     }
     function fetchGoalsFromDisk() {
         var fetched_goals = localStorage.getItem('doit:goals');
@@ -68,12 +146,15 @@ dashApp.service('GoalService', function($http) {
     return {
         addGoal: addGoal,
         listGoals: listGoals,
-        clearGoals: clearGoals
+        clearGoals: clearGoals,
+        addActivityOnGoal: addActivityOnGoal,
+        removeActivityOnGoal: removeActivityOnGoal
     };
 });
 
 dashApp.controller('DashboardController', function($scope, GoalService) {
     $scope.STATE_ADD_GOAL = 'ADD_GOAL';
+    $scope.STATE_ADD_ACTIVITY_ON_DATE = 'ADD_ACTIVITY_ON_DATE';
     $scope.state = null;
 
     $scope.showAddGoal = function() {
@@ -91,6 +172,43 @@ dashApp.controller('DashboardController', function($scope, GoalService) {
     $scope.clearGoals = function() {
         GoalService.clearGoals();
         init();
+    };
+
+    $scope.addActivity = function(goal) {
+        if (GoalService.addActivityOnGoal(goal)) {
+            console.log('Added!');
+        } else {
+            console.log('Not added activity');
+        }
+    };
+
+    $scope.removeActivity = function(goal) {
+        if (GoalService.removeActivityOnGoal(goal)) {
+            console.log('Removed!');
+        } else {
+            console.log('Not removed');
+        }
+    };
+
+    $scope.showAddActivityOnDate = function(goal) {
+        $scope.state = $scope.STATE_ADD_ACTIVITY_ON_DATE;
+        $scope.add_activity_model = {
+            goal: goal
+        };
+    };
+
+    $scope.cancelAddActivityOnDate = function() {
+        $scope.state = null;
+    };
+
+    $scope.addActivityOnDate = function() {
+        if (!($scope.add_activity_model.date instanceof Date)) {
+            return;
+        }
+        if (!GoalService.addActivityOnGoal($scope.add_activity_model.goal, $scope.add_activity_model.date)) {
+            console.log('Already have activity on date. Ignoring');
+        }
+        $scope.state = null;
     };
 
     function init() {
